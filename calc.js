58b4050e5f095746690522d15eab127cba60250f
ワイくん <105576113+himazin355@users.noreply.github.com>
2022-07-21 17:34:16 +0900

    ソースコード整形

--- calc.js
+++ calc.js
@@ -7,51 +7,59 @@ let r = localStorage.getItem("Result");
 if (r != null) result.innerHTML = r;
 let a = localStorage.getItem("Answer");
 if (a != null) answer = a;
-$("#formula").on("input", function (e) {
+$("#formula").on("input", function(e) {
   localStorage.setItem("Formula", formula.value);
 });
+
 function add(elem) {
   formula.value += elem.value;
   localStorage.setItem("Formula", formula.value);
 }
+
 function HEX() {
-	let ans = parseInt(formula.value);
-	let HEX = ans.toString(16);
-	localStorage.setItem("Result", HEX);
-	result.innerHTML =  "= " + HEX;
+  let ans = parseInt(formula.value);
+  let HEX = ans.toString(16);
+  localStorage.setItem("Result", HEX);
+  result.innerHTML = "= " + HEX;
 }
+
 function OCT() {
-	let ans = parseInt(formula.value);
-	let OCT = ans.toString(8);
-	localStorage.setItem("Result", OCT);
-	result.innerHTML =  "= " + OCT;
+  let ans = parseInt(formula.value);
+  let OCT = ans.toString(8);
+  localStorage.setItem("Result", OCT);
+  result.innerHTML = "= " + OCT;
 }
+
 function BIN() {
-	let ans = parseInt(formula.value);
-	let BIN = ans.toString(2);
-	localStorage.setItem("Result", BIN);
-	result.innerHTML =  "= " + BIN;
+  let ans = parseInt(formula.value);
+  let BIN = ans.toString(2);
+  localStorage.setItem("Result", BIN);
+  result.innerHTML = "= " + BIN;
 }
+
 function del() {
   let f = formula.value;
   formula.value = f.substring(0, f.length - 1);
   localStorage.setItem("Formula", formula.value);
 }
+
 function ac() {
   formula.value = "";
   localStorage.setItem("Formula", formula.value);
   localStorage.setItem("Result", "");
   result.innerHTML = "";
 }
+
 function cal() {
   let f = formula.value;
-let s = 0, e = 0;
-  Array.prototype.forEach.call(f, function (c) {
+  let s = 0,
+    e = 0;
+  Array.prototype.forEach.call(f, function(c) {
     if (c == "(") s++;
     if (c == ")") e++;
   });
-  for (let i = 0; i < s - e; i++)f += ")";
- f = f.replace(/\^/g, "**");
+  for (let i = 0; i < s - e; i++) f += ")";
+  f = f.replace(/\^/g, "**");
   f = f.replace(/deg/g, "180/pi");
   f = f.replace(/rad/g, "pi/180");
   f = f.replace(/pi/g, "Math.PI");
@@ -66,11 +74,10 @@ let s = 0, e = 0;
     if (Number.isNaN(r)) throw Error;
     result.innerHTML = "= " + r;
     answer = r;
-  }
-  catch (e) {
+  } catch (e) {
     result.innerHTML = "<span><strong>error</strong></span>";
   }
-	localStorage.setItem("Result", result.innerHTML);
+  localStorage.setItem("Result", result.innerHTML);
   localStorage.setItem("Answer", answer);
   return false;
 }
